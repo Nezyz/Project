8ee507e412bdf505c35cea1cac42eb949eaab62f
yandex <ProGlot003@yandex.ru>
2018-12-06 17:55:45 +0300

    Изменения

--- Converter.py
+++ Converter.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5.QtWidgets import QInputDialog
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('int.ui', self)
@@ -14,31 +14,39 @@ class Example(QWidget):
         self.setGeometry(200, 200, 550, 550)
         self.setWindowTitle('Диалоговые окна')
         self.button_3 = QPushButton(self)
-        self.button_3.move(20, 70)
-        self.button_3.setText("Выберите основную единицу веса")
+        self.button_3.move(60, 100)
+        self.button_3.setText("Единица")
         self.button_3.clicked.connect(self.run3)
         self.button_4 = QPushButton(self)
-        self.button_4.move(20, 150)
+        self.button_4.move(60, 250)
         self.button_4.setText("Результат")
         self.button_4.clicked.connect(self.run4)
+        self.from_ = 0
+        self.to_ = 0
 
         self.show()
 
     def run3(self):
         i, okBtnPressed = QInputDialog.getItem(self, "Выберите единицу измериения", "Единицы измерения",
-                                               ("Тонны", "Граммы", "Килограммы"), 2, False)
+                                               ("Тонны", "Килограммы", "Граммы"), 2, False)
         if okBtnPressed:
             self.button_3.setText(i)
+            self.from_ = i
+
 
     def run4(self):
         i, okBtnPressed = QInputDialog.getItem(self, "Выберите единицу измериения", "Единицы измерения",
-                                               ("Тонны", "Граммы", "Килограммы"), 2, False)
+                                               ("Тонны", "Килограммы", "Граммы"), 2, False)
         if okBtnPressed:
-            self.button_4.setText(i)
+            self.to_ = i
+
+            s = {"Тонны": 1000000, "Килограммы": 1000, "Граммы": 1}
+
+            self.button_4.setText(str(float(self.lineEdit.text())*s[self.from_]/s[self.to_]))
 
 
 app = QApplication(sys.argv)
+
 ex = Example()
 ex.show()
 sys.exit(app.exec_())
-
